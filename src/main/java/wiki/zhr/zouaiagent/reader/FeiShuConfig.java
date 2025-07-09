1b4cd5c48bfed2f7d12c4987dc6f4c3ab0aeb10a
zouhaoran.2025 <zouhaoran.2025@bytedance.com>
2025-07-09 11:54:10 +0800

    增加本地+云RAG知识库

--- src/main/java/wiki/zhr/zouaiagent/reader/FeiShuConfig.java
+++ src/main/java/wiki/zhr/zouaiagent/reader/FeiShuConfig.java
@@ -4,8 +4,8 @@ package wiki.zhr.zouaiagent.reader;
  * 这这里输入你的飞书配置
  */
 public interface FeiShuConfig {
-    String FEISHU_APP_ID = "cli_a8e76d199338900e";
-    String FEISHU_APP_SECRET = "2RI6RM8oKiSp38E4brBMtd8TGO43w5cz";
+    String FEISHU_APP_ID = "";
+    String FEISHU_APP_SECRET = "";
     String FEISHU_USER_TOKEN = "";
     String FEISHU_DOCUMENT_ID = "";
 }
